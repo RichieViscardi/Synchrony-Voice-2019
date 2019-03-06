858f2ad21401d2ee79ee1e55fb08b9f916f0f8be
Connor <test>
2019-03-06 14:15:54 -0500

    payment

--- app/src/main/java/com/example/seniordesign/BaseFragment.java
+++ app/src/main/java/com/example/seniordesign/BaseFragment.java
@@ -1,10 +1,30 @@
 package com.example.seniordesign;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 /**
  * Generalized Fragment throughout app.
  * Any actions that should persist throughout entire app will live here.
  */
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(getContentView(), container, false);
+        return view;
+    }
+
+    /**
+     * Method to get the layout id from child in order to inflate the layout
+     * @return It will return layout id
+     */
+    protected abstract int getContentView();
 }
